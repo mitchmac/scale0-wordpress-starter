b38415a446e3f61385716b45fa67f35bbc02a321
Mitch MacKenzie <mitchmac@gmail.com>
2023-04-26 14:35:26 -0300

    Test hack of wpdb for persistent connections

--- wp/wp-includes/class-wpdb.php
+++ wp/wp-includes/class-wpdb.php
@@ -2019,7 +2019,7 @@ class wpdb {
 				mysqli_real_connect( $this->dbh, $host, $this->dbuser, $this->dbpassword, null, $port, $socket, $client_flags );
 			} else {
 				// phpcs:ignore WordPress.PHP.NoSilencedErrors.Discouraged
-				@mysqli_real_connect( $this->dbh, $host, $this->dbuser, $this->dbpassword, null, $port, $socket, $client_flags );
+				@mysqli_real_connect( $this->dbh, 'p:' . $host, $this->dbuser, $this->dbpassword, null, $port, $socket, $client_flags );
 			}
 
 			if ( $this->dbh->connect_errno ) {
